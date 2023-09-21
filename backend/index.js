2a6d6efa3bb15b47f62e8b297ac4f618a3b6ad20
Fernando P.S.D <it21085444@my.sliit.lk>
2023-09-21 09:25:21 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import {PORT, mongoDBURL} from "./config.js"
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling CORS policy
+//Option 1: Allow all origins with default cors
+//app.use(cors());
+
+//option 2: allow custom origins
+app.use(
+    cors({
+        origin: "http://localhost:5555",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+    );
+app.get('/', (request, response) => { //callback function handles get requests
+    console.log(request)
+    return response.status(234).send('welcome to mern turorial')
+});
+
+app.use('/books' , booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is running on post : ${PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
